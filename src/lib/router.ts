f762fd8354f894edb40fdec77257fd325abf3943
James-Guo-03 <125829890+James-Guo-03@users.noreply.github.com>
2025-12-22 22:19:12 -0500

    restore change of the router

--- src/lib/router.ts
+++ src/lib/router.ts
@@ -1,10 +1,10 @@
 import { createRouter } from "@nanostores/router";
 
-const BASE_URL = "/homomorphism-convergence";
+const BASE_URL = "/homomorphism-convergence/";
 
 export const $router = createRouter({
-    home: `${BASE_URL}/`,
-    characters: `${BASE_URL}/chars`,
-    algebra: `${BASE_URL}/algebra`,
-    analysis: `${BASE_URL}/analysis`,
+    home: `${BASE_URL}`,
+    characters: `${BASE_URL}chars`,
+    algebra: `${BASE_URL}algebra`,
+    analysis: `${BASE_URL}analysis`,
 });
