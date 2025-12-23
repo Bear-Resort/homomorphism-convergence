f9e24e8babad4e422d0c46eb96b5bed21bffa0a3
James-Guo-03 <125829890+James-Guo-03@users.noreply.github.com>
2025-12-22 22:12:21 -0500

    new attempt on debug

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// import tailwindcss from '@tailwindcss/vite'
 
 import path from "path";
  
@@ -9,7 +9,7 @@ export default defineConfig({
   base: "/homomorphism-convergence/",
   plugins: [
     react(),
-    tailwindcss(),
+    // tailwindcss(),
   ],
   resolve: {
     alias: {
